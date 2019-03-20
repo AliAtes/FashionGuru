68ec708aad69f9d883039a78b8d010ef9726f191
AliAtes <ali.ates@outlook.com>
2019-03-20 10:18:56 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,6 +9,7 @@ import base64
 import json
 import urllib3
 from bs4 import BeautifulSoup
+import logging
 
 model_file_url = 'https://github.com/AliAtes/DeepFashionKTE/blob/master/app/models/model.pth?raw=true'
 model_file_name = 'model'
@@ -45,16 +46,22 @@ PREDICTION_FILE_SRC = path/'static'/'predictions.txt'
 async def upload(request):
     data = await request.form()
     img_bytes = (data["img"])
+    logging.warning("data[img]: " + str(data["img"]))
     bytes = base64.b64decode(img_bytes)
     return predict_from_bytes(bytes)
 
 def predict_from_bytes(bytes):
     img = open_image(BytesIO(bytes))
+    
     _,_,losses = learn.predict(img)
     predictions = sorted(zip(classes, map(float, losses)), key=lambda p: p[1], reverse=True)
+    logging.warning("predictions[0]: " + str(predictions[0]))
+    
     http = urllib3.PoolManager()
     page = http.request('GET', 'https://www.google.com/search?q=' + str(predictions[0]) + '&tbm=shop')
+    
     soup = BeautifulSoup(page.data, 'html.parser')
+    
     all_cards_html = ""	
     for tag in soup.find_all("div", attrs={'class': 'pslires'}):
     	link = tag.div.a['href']
@@ -63,6 +70,9 @@ def predict_from_bytes(bytes):
     	shop = tag.findChildren()[6].getText()
     	amount = tag.findChildren()[3].div.getText()
     	all_cards_html += ("<div class=\"col-6 col-sm-6 col-md-4 col-lg-3\" style=\"margin-top:20px;\"><a href=\"" + link + "\"><table style=\"text-align:center;\"><tr><td><img src=\"" + image + "\" /></td></tr><tr><td>" + info + " | " + shop + "</td></tr><tr><td>" + amount + "</td></tr></table></a></div>")
+    
+    logging.warning("all_cards_html: " + all_cards_html)
+    
     result_html1 = path/'static'/'result1.html'
     result_html2 = path/'static'/'result2.html'
     result_html = str(result_html1.open().read() + all_cards_html + result_html2.open().read())
