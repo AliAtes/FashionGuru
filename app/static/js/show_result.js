4aee1bf9c23a6abbf9e8f535a3d3fd38750b9e0b
AliAtes <ali.ates@outlook.com>
2019-03-20 11:32:29 +0300

    Update show_result.js

--- app/static/js/show_result.js
+++ app/static/js/show_result.js
@@ -4,4 +4,3 @@ var el = x => document.getElementById(x);
 var dataImage = localStorage.getItem('imgData');
 saved_image = el('saved_image');
 saved_image.src = "data:image/jpeg;base64," + dataImage;
-alert('saved_image.src: ' + saved_image.src);
