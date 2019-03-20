028454d5ebb65fdeeab2f549176c875a23db5b4a
AliAtes <ali.ates@outlook.com>
2019-03-20 23:03:46 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -80,7 +80,7 @@ def predict_from_bytes(bytes, radios):
     	amount = tag.findChildren()[3].div.getText()
     	all_cards_html += ("<div class=\"col-6 col-sm-6 col-md-4 col-lg-3 col-centered text-center card\"><div><a href=\"" + link + "\"><img style=\"width:100%; margin-bottom:5px;\" src=\"" + image + "\" /></a></div><div>" + info + "<br />" + shop + "<br />" + amount + "</div></div>")
     
-    all_cards_html += ("<!-- predictions: { " + "[" + predictions[0][0] + " - " + predictions[0][1] + "], " + "[" + predictions[1][0] + " - " + predictions[1][1] + "], " + "[" + predictions[2][0] + " - " + predictions[2][1] + "]" + " }-->")
+    all_cards_html += ("<!-- predictions: { " + "[" + str(predictions[0][0]) + " - " + str(predictions[0][1]) + "], " + "[" + str(predictions[1][0]) + " - " + str(predictions[1][1]) + "], " + "[" + str(predictions[2][0]) + " - " + str(predictions[2][1]) + "]" + " }-->")
     logging.warning("all_cards_html: " + all_cards_html)
     
     result_html1 = path/'static'/'result1.html'
