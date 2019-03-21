737c8e8aa782d5022289ea71b3250517f781854c
AliAtes <ali.ates@outlook.com>
2019-03-22 00:26:09 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -82,7 +82,7 @@ def predict_from_bytes(bytes, radios):
     prediction_tr1 = classes_tr[classes.index(predictions[0][0])]
     prediction_tr2 = classes_tr[classes.index(predictions[1][0])]
     prediction_tr3 = classes_tr[classes.index(predictions[2][0])]
-    analysis = ("{ " + radios + " " + str(predictions[0][0]) + " (" + radios_tr + " " + prediction_tr1 + ") : [" + str(predictions[0][1]) + "] }" + "<br>" + "{ " + radios + " " + str(predictions[1][0]) + " (" + radios_tr + " " + prediction_tr2 + ") : [" + str(predictions[1][1]) + "] }" + "<br>" + "{ " + radios + " " + str(predictions[2][0]) + " (" + radios_tr + " " + prediction_tr3 + ") : [" + str(predictions[2][1]) + "] }")
+    analysis = ("{ " + radios + " " + str(predictions[0][0]) + " (" + radios_tr + " " + prediction_tr1 + ") : [" + str(round(predictions[0][1],2) + "] }" + "<br>" + "{ " + radios + " " + str(predictions[1][0]) + " (" + radios_tr + " " + prediction_tr2 + ") : [" + str(round(predictions[1][1],2)) + "] }" + "<br>" + "{ " + radios + " " + str(predictions[2][0]) + " (" + radios_tr + " " + prediction_tr3 + ") : [" + str(round(predictions[2][1],2)) + "] }")
     logging.warning(analysis)
     
     radiosAndPrediction = quote(radios + " " + str(predictions[0][0]))
