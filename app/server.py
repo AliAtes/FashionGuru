48c39fcc0b2802d5c1caffef753c3ebd02d74603
AliAtes <ali.ates@outlook.com>
2019-03-21 20:33:50 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -15,6 +15,7 @@ from urllib.parse import quote
 model_file_url = 'https://github.com/AliAtes/DeepFashionKTE/blob/master/app/models/model.pth?raw=true'
 model_file_name = 'model'
 classes = ['Blouse', 'Blazer', 'Button-Down', 'Bomber', 'Anorak', 'Tee', 'Tank', 'Top', 'Sweater', 'Flannel', 'Hoodie', 'Cardigan', 'Jacket', 'Henley', 'Poncho', 'Jersey', 'Turtleneck', 'Parka', 'Peacoat', 'Halter', 'Skirt', 'Shorts', 'Jeans', 'Joggers', 'Sweatpants', 'Jeggings', 'Cutoffs', 'Sweatshorts', 'Leggings', 'Culottes', 'Chinos', 'Trunks', 'Sarong', 'Gauchos', 'Jodhpurs', 'Capris', 'Dress', 'Romper', 'Coat', 'Kimono', 'Jumpsuit', 'Robe', 'Caftan', 'Kaftan', 'Coverup', 'Onesie']
+classes_tr = ['bluz', 'blazer ceket', 'düğmeli giysi', 'bomber ceket', 'anorak', 'tişört', 'sporcu atleti', 'üst', 'süveter', 'flanel gömlek', 'svetşört', 'uzun hırka', 'mont', 'triko tişört', 'panço', 'jarse', 'balıkçı yaka', 'parka', 'palto', 'askılı', 'etek', 'şort', 'kot', 'pantolon eşofman', 'eşofman altı', 'dar kesim pantolon', 'kot şort', 'sweatshorts', 'tayt', 'pantolon etek', 'chino pantolon', 'erkek mayosu', 'sarong', 'bol paça kısa pantolon', 'binicilik pantolonu', 'kapri', 'elbise', 'tulum', 'kaban', 'kimono', 'bağcıklı tulum', 'sabahlık', 'kaftan', 'kaftan', 'şal elbise', 'pijama kostüm']
 
 path = Path(__file__).parent
 
@@ -69,9 +70,23 @@ def predict_from_bytes(bytes, radios):
     #elif(radios == "kadin"): radios = "kadın"
     #elif(radios == "cocuk"): radios = "çocuk"
     
-    if(radios == "erkek"): radios = "men"
-    elif(radios == "kadin"): radios = "women"
-    elif(radios == "cocuk"): radios = "boy"
+    radios_tr = ""
+    if(radios == "Men"): radios_tr = "Erkek"
+    elif(radios == "Women"): radios_tr = "Kadın"
+    elif(radios == "Boy"): radios_tr = "Çocuk"
+        
+    
+    # { Men Tee (Erkek Tişört) : [0.98546] }
+    # { Men Top (Erkek Üst) : [0.0123455] }
+    # { Men Caftan (Erkek Kaftan) : [0.001324] }
+    
+    prediction_tr1 = classes_tr[classes.index(predictions[0][0])]
+    prediction_tr2 = classes_tr[classes.index(predictions[1][0])]
+    prediction_tr3 = classes_tr[classes.index(predictions[2][0])]
+    analysis = str("{ " + radios + " " + predictions[0][0] + " (" + radios_tr + " " + prediction_tr1 + ") : [" + predictions[0][1] + "] }" + "<br>" +
+    "{ " + radios + " " + predictions[1][0] + " (" + radios_tr + " " + prediction_tr2 + ") : [" + predictions[1][1] + "] }" + "<br>" +
+    "{ " + radios + " " + predictions[2][0] + " (" + radios_tr + " " + prediction_tr3 + ") : [" + predictions[2][1] + "] }")
+    
     
     radiosAndPrediction = quote(str(radios) + " " + str(predictions[0][0]))
     logging.warning(radiosAndPrediction)
