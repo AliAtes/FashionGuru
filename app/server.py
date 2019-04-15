89cc8234bfe186660fa050c218faa2346450dc21
AliAtes <ali.ates@outlook.com>
2019-04-15 13:17:03 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -60,7 +60,7 @@ async def upload(request):
     if(img_ori == 3):
     	img = img.rotate(-180)
     if(img_ori == 6):
-    	img = img.rotate(180)
+    	img = img.rotate(-90)
     elif(img_ori == 8):
     	img = img.rotate(90)
     
