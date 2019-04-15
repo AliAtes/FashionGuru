817a004f00f9fe9d10514401da7bd25e85027373
AliAtes <ali.ates@outlook.com>
2019-04-15 10:03:39 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -57,11 +57,11 @@ async def upload(request):
     img = Image.open(BytesIO(base64.b64decode(img_base64)))
     
     if(img_ori == 3):
-        img = img.rotate(180)
-	elif(img_ori == 6):
-	    img = img.rotate(90)
-	elif(img_ori == 8):
-	    img = img.rotate(-90)
+    	img = img.rotate(180)
+    elif(img_ori == 6):
+	img = img.rotate(90)
+    elif(img_ori == 8):
+        img = img.rotate(-90)
     
     logging.warning("img: " + str(img))
     logging.warning("img_ori: " + img_ori)
